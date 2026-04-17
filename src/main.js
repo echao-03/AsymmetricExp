7600eaca93d42eccf43ecdc284a8637d280487e2
Erik Chao <etchao001@gmail.com>
2026-04-16 20:45:19 -0700

    adding blink locomotion

--- src/main.js
+++ src/main.js
@@ -205,9 +205,10 @@ function renderInContainer(cam, el) {
   renderer.setScissor(x, y, w, h);
   renderer.render(scene, cam);
 }
-
+const clock = new THREE.Clock();
 renderer.setAnimationLoop(() => {
-  movement.update();
+  const delta = clock.getDelta();
+  movement.update(delta);
   controls.update();
   multiplayer.updatePose(VRCamera, leftController, rightController, playerRig);
 
